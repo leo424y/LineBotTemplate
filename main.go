0aaf1317a04608cf65aaefe0c2b68c3940555714
Yang-Hsiang Chang <leo424y@gmail.com>
2017-11-01 16:13:02 +0800

    .

--- main.go
+++ main.go
@@ -13,46 +13,46 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+  "fmt"
+  "log"
+  "net/http"
+  "os"
 
-	"github.com/line/line-bot-sdk-go/linebot"
+  "github.com/line/line-bot-sdk-go/linebot"
 )
 
 var bot *linebot.Client
 
 func main() {
-	var err error
-	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
-	log.Println("Bot:", bot, " err:", err)
-	http.HandleFunc("/callback", callbackHandler)
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
-	http.ListenAndServe(addr, nil)
+  var err error
+  bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
+  log.Println("Bot:", bot, " err:", err)
+  http.HandleFunc("/callback", callbackHandler)
+  port := os.Getenv("PORT")
+  addr := fmt.Sprintf(":%s", port)
+  http.ListenAndServe(addr, nil)
 }
 
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	events, err := bot.ParseRequest(r)
+  events, err := bot.ParseRequest(r)
 
-	if err != nil {
-		if err == linebot.ErrInvalidSignature {
-			w.WriteHeader(400)
-		} else {
-			w.WriteHeader(500)
-		}
-		return
-	}
+  if err != nil {
+    if err == linebot.ErrInvalidSignature {
+      w.WriteHeader(400)
+    } else {
+      w.WriteHeader(500)
+    }
+    return
+  }
 
-	for _, event := range events {
-		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
-					log.Print(err)
-				}
-			}
-		}
-	}
+  for _, event := range events {
+    if event.Type == linebot.EventTypeMessage {
+      switch message := event.Message.(type) {
+      case *linebot.TextMessage:
+        if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("你提供的資訊是："+message.Text+"  謝謝你，我收到囉！明早向你報告統計結果")).Do(); err != nil {
+          log.Print(err)
+        }
+      }
+    }
+  }
 }
